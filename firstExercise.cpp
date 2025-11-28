7c83370aae29dce7b5264f8fea81a48ae314da60
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2025-11-28 23:28:16 +0100

    First commit in C++

---
+++ firstExercise.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+  string ime, prezime;
+  int godine;
+  cout << "Unesi ime:";
+  cin >> ime;
+  cout << "Unesi prezime:";
+  cin >> prezime;
+  cout << "Unesi godine: ";
+  cin >> godine;
+  cout << "Ime i prezime:" << ime << " "<< prezime << "\ngodine: " << godine;
+
+  /*
+  int firstNum, secondNum;
+  cout << "Unesite prvi broj: ";
+  cin >> firstNum;
+  cout << "Unesite drugi broj: ";
+  cin >> secondNum;
+  int zbir = firstNum + secondNum;
+  int razlika = firstNum - secondNum;
+  double kolicnik = firstNum % secondNum;
+  int proizvod = firstNum * secondNum;
+  int celobrojniKolicnik = firstNum / secondNum;
+  cout << "Zbir= " << zbir << endl; 
+  cout << "Razlika= " << Razlika << endl; 
+  cout << "Kolicnik= " << kolicnik << endl; 
+  cout << "Proizvod= " << proizvod << endl; 
+  cout << "Celobrojni kolicnik= " << celobrojniKolicnik << endl; 
+  */
+  /*
+  string ime, prezime;
+  cout << "Unesite ime i prezime:";
+  cin >> ime >> prezime;
+  cout << "Ime: "<< ime << endl << "Prezime: "<< prezime;
+  */
+
+  /*
+  int a,b;
+  cout <<"Unesite 2 broja: ";
+  cin >> a >> b;
+  cout << "Broj a: " << a << endl << "Broj b: "<< b;
+  */
+
+  /*
+  int x = 5;
+  double y = 10;
+  double z = (x+10) / (3*y); 
+  cout << "Prvi broj:"<< x << endl << "Drugi broj:"<< y;
+  */
+
+  return 0;
+}
