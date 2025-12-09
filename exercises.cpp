a6d24f8fac9ae63017e00d80cfa029e8c48178fb
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2025-12-09 21:48:34 +0100

    Examples for midterm

---
+++ exercises.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  int h, w;
+  cout << "Enter num of rows: ";
+  cin >> h;
+  cout << "Enter num of asterisks in the last row: ";
+  cin >> w;
+  if (h < 3 || h > 20 || w < 3 || w > 60)
+  {
+    cout << "Invalid parametars";
+  }else
+  {
+    for (int i = 1; i <= w; i++) cout << "*";
+    cout << endl;
+    for (int j = 2; j <= h-1; j++)
+    {
+      for (int i = 1; i <= j-1; i++) cout << " ";
+      cout << "*";
+      for (int i = 1; i <= w-2; i++) cout << " ";
+      cout << "*" << endl;
+    }
+    for (int i = 1; i <= h-1; i++) cout << " ";
+    for (int i = 1; i <= w; i++) cout << "*";
+    cout << endl;
+  }
+  
+  
+  /*
+  int h, w;
+  cout << "Enter num of rows: ";
+  cin >> h;
+  cout << "Enter num of asterisks in the last row: ";
+  cin >> w;
+  if (h < 3 || h > 20 || 2*h+3 > w || w > 70)
+  {
+    cout << "Invalid parameters";
+  }else
+  {
+    for (int i = 1; i <= h-1; i++) cout << " ";
+    for (int i = 1; i <= w-2*h+2; i++) cout << "*";
+    cout << endl;
+    for (int j = 2; j <= h-1; j++)
+    {
+      for (int i = 1; i <= h-j; i++) cout << " ";
+      cout << "*";
+      for (int i = 1; i <=  w-2*h-2+2*j; i++) cout << " ";
+      cout << "*" << endl;
+    }
+    for (int i = 1; i <= w; i++) cout <<"*";
+    cout << endl;
+  }
+  */
+  
+  /*
+  int h, w;
+  cout << "Enter num of rows: ";
+  cin >> h;
+  cout << "Enter num of asterisks in the last row: ";
+  cin >> w;
+  
+  if (h < 3 || h > 20 || w < 2*h+3 || w > 70)
+  {
+    cout << "Invalid parametars.";
+  }else
+  {
+    for (int i = 1; i < h; i++)
+    {
+      for (int j = 1; j < h-i; j++) cout << " ";
+      for (int j = 1; j < w-2*h+2*i; j++) cout << "*";
+      cout << endl;
+    }
+  }
+  */
+  
+  return 0;
+}
