fc32ac5208fbdf816838de593b97586af23ce8e1
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2026-01-28 19:19:48 +0100

    Preparation for final exam

--- preparationForFinal.cpp
+++ preparationForFinal.cpp
@@ -18,30 +18,183 @@ bool isPrime(int x){
   }
   return true;
 }
+
+bool isPerfect(int x){
+  int sumOfDiv = 0;
+  for (int i = 1; i < x; i++)
+  {
+    if(x % i == 0) sumOfDiv += i;
+  }
+  
+  if(sumOfDiv == x){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void minMaxDigit(int x, int &minDigit, int &maxDigit){
+  minDigit = 9, maxDigit = 0;
+
+  while(x != 0){
+  int digit = x % 10;
+    if(digit > maxDigit) maxDigit = digit;
+    if(digit < minDigit) minDigit = digit;
+    x /= 10;
+  }
+}
+bool isMonoIncreasing(int arr[], int size){
+  for (int i = 1; i < size; i++)
+  {
+    if(arr[i] < arr[i-1]) return false;
+  }
+  return true;
+}
+int longestWordLength(string s){
+  int currentLength = 0, maxLength = 0;
+  for (int i = 0; i < s.length(); i++)
+  {
+    if(s.at(i) != ' '){
+      currentLength++;
+    }else{
+      if(currentLength > maxLength){
+        maxLength = currentLength;
+      }
+      currentLength = 0;
+    } 
+  }
+  if(currentLength > maxLength) maxLength = currentLength;
+  return maxLength;
+}
 int main (){
+  string s;
+  getline(cin, s);
+  cout << "Length of the longest word: " << longestWordLength(s) << endl;
+  /*
+  int arr[5];
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> arr[i];
+  }
+  if(isMonoIncreasing(arr,5)){
+    cout << "Arr is monotonically increasing!";
+  }else{
+    cout << "Arr isn't monotonically increasing!";
+  }
+  */
+  
+
+  /*
   int n;
   cin >> n;
+  
+  for (int i = 2; i+2 < n; i++)
+  {
+    if(isPrime(i) && isPrime(i+2)){
+      cout << "(" << i << ", " << i+2 << ")";
+    }
+  }
+  */
+  
+
+  /*
+  int n;
+  cin >> n;
+  for (int i = 2; i < n; i++)
+  {
+    if(isPrime(i) && n % i == 0){
+      cout << i << " ";
+    }
+  }
+  */
+
+  /*
+  int h, w;
+  cout << "Enter the height: ";
+  cin >> h;
+  cout << "Enter the width: ";
+  cin >> w;
+  
+  if(h < 3 || h > 20 || w < 2*h+3 || w > 70){
+    cout << "Invalid input!!!";
+  }else{
+
+    for (int i = 0; i < h-1; i++) cout << " ";
+    for (int i = 0; i < 2*h+2; i++) cout << "+";
+    cout << endl;
+    
+    for (int i = 1; i < h-1; i++)
+    {
+      for (int j = 1; j < h-i; j++) cout << " ";
+      cout << "+";    
+      for (int j = 1; j <= 2*h+2*i; j++) cout << " ";
+      cout << "+"<< endl;   
+    }
+    for (int i = 0; i < w; i++) cout << "+";
+  }
+  */
+
+  /*
+  int n, positive = 0, negative = 0; 
+  for (int i = 0; i < 10; i++)            
+  {                              
+    cin >> n;
+    if(n > 0) positive++;
+    if(n < 0) negative++;
+  }                            
+  cout << "There are " << positive << " positive nums and " << negative << "negatives num."; 
+  */
+
+  /*
+  int n, min, max;
+  cin >> n;
+  minMaxDigit(n, min, max);
+  cout << "Num: " << n << ", minDigit " << min << ", maxDigit " << max;
+  */
+
+  /*
+  for (int i = 1; i < 100; i++)
+  {
+    if(isPerfect(i)) cout << i << " ";
+  }
+  */
+
+  /*
+  int n;
+  cin >> n;
+  
   if(n < 4 || n > 20){
     cout << "Invalid input!!!";
   }else{
-    for(int j = 1; j <= (N - 1) / 2; j++) cout << " ";      // top
- cout << "*" << endl;
- for(int i = 1; i <= (N - 1) / 2; i++){ // upper half
-     for(int j = 1; j <= (N - 1) / 2 - i; j++) cout << " ";
-     cout << "*";
-     for(int j = 1; j <= 2 * i - 1; j++) cout << " ";
-     cout << "*" << endl;
- }
- for(int i = (N - 2) / 2; i >= 1; i--){         // lower half
-     for(int j = 1; j <= (N - 1) / 2 - i; j++) cout << " ";
-     cout << "*";
-     for(int j = 1; j <= 2 * i - 1; j++) cout << " ";
-     cout << "*" << endl;
- }
- for(int j = 1; j <= (N - 1) / 2; j++) cout << " ";         // bottom
- cout << "*" << endl
-
+    int mid = n/2;
+    
+    for(int i = 0; i < mid; i++) cout << " ";
+    cout << "*" << endl;
+    
+    for (int i = 1; i <= mid; i++){
+      for (int j = 1; j <= mid-i; j++) cout << " ";
+      cout << "*";
+      if (i >= 1) {
+        for (int j = 1; j <= 2*(i-1)+1; j++) cout << " ";
+        cout << "*";
+      }
+      cout << endl;
+    }
+    
+    for (int i = mid-1; i >= 1; i--) {
+      for (int j = 1; j <= mid-i; j++) cout << " ";
+      cout << "*";
+      if (i >= 1) {
+        for (int j = 1; j <= 2*(i-1)+1; j++) cout << " ";
+        cout << "*";
+      }
+      cout << endl;
+    }
+    
+    for (int i = 0; i < mid; i++) cout << " ";
+    cout << "*" << endl;
   }
+  */
 
   /*
   int m, n;
