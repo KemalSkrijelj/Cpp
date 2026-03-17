9ea23a4f1738b57dd9f8401c013e2a557077b926
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2026-03-17 23:52:05 +0100

    Examples

--- pointers.cpp
+++ pointers.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+int subtraction(int *x, int *y){
+  *x =  *y- *x;
+  return *x;
+}
+void value(int a){ //not work this func
+  a = 15; 
+}
+void reference(int *p){
+  *p = 80;
+}
+
 int main(){
+  int x = 5;
+  int y = 7;
+  cout << "x:" << x << endl;
+  cout << "y:" << y << endl;
+  value(x); // not work this func because this func don't have access for this variable
+  reference(&y);
+  cout << "Substraction value: " << subtraction(&x, &y) << endl;
+  cout << "New x:" << x << endl;
+  cout << "New y:" << y << endl;
+
+
+  /*
   string name = "Kemal";
   int age = 20;
   string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
@@ -13,10 +36,11 @@ int main(){
   int *pAge = &age;
   string *pfreePizzas = freePizzas;
   int *pointer = &x;
-
+  
   cout << *pName << endl;
   cout << *pAge << endl;
   cout << *pfreePizzas << endl;
   cout << *pointer;
+  */
   return 0;
 }
