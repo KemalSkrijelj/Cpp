c1774b258f28a7cbe3241c1ebc06f6e980d873d4
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2026-04-11 22:16:25 +0200

    Classes examples

--- classes.cpp
+++ classes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <stdexcept>
 
 using namespace std;
 
@@ -111,7 +112,143 @@ class Student{
   }
 };
 
+class BankAccount{
+  double balance;
+  public:
+  BankAccount(){
+    balance = 0;
+  }
+  BankAccount(double initialAmount){
+    if(initialAmount <= 0){
+      throw domain_error("Inital amout mst be greather than 0!");
+    }
+    balance = initialAmount;
+  }
+  void Deposit(double amountOfDeposit){
+    if(amountOfDeposit <= 0){
+      throw domain_error("Bad deposit!!");
+    }
+    balance += amountOfDeposit;
+  }
+  void Withdraw(double amountOfWithdraw){
+    if(amountOfWithdraw <= 0  || amountOfWithdraw > balance){
+      throw domain_error( "Bad withdraw!!");
+    }
+    balance -= amountOfWithdraw;
+  }
+  void AddInterest(double interestRate){
+    if(interestRate <= 0){
+      throw domain_error("Bad interest rate!!");
+    }
+    balance += balance * interestRate;
+  }
+  double GetBalance() const {
+    return balance;
+  }
+  void Display(){
+    cout << "Balance:" << balance << endl;
+  }
+};
+
+
+
+class Triangle{
+  double a,b,c;
+  public:
+  Triangle(double side1, double side2, double side3){
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2)
+    {
+      throw invalid_argument("Invalid input of sides!");
+    }
+    a = side1;
+    b = side2;
+    c = side3;
+  }
+  void setTriangle(double side1, double side2, double side3){
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2)
+    {
+      throw invalid_argument("Invalid input of sides!");
+    }
+    a = side1;
+    b = side2;
+    c = side3;
+  }
+  double getA() const { return a; };
+  double getB() const { return b; };
+  double getC() const { return c; };
+  double getCircumference() const{
+    return a+b+c;
+  }
+  double getArea() const {
+  double s = getCircumference() / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+  }
+  bool isRightAngled(){
+    if(pow(a, 2) == pow(b, 2) + pow(c, 2) ||
+       pow(b, 2) == pow(c, 2) + pow(a, 2) ||
+       pow(c, 2) == pow(a, 2) + pow(b, 2)) {
+        return true;
+    }
+    return false;
+  }
+  void scalingTriangle(double factor){
+    if (factor <= 0)
+    {
+      throw invalid_argument("Invalid factor!");
+    }
+    a *= factor;
+    b *= factor;
+    c *= factor;
+  }
+  void Display(){
+    cout << "Triangle: " << endl
+    << "A: " << getA() << endl
+    << "B: " << getB() << endl
+    << "C: " << getC() << endl
+    << "Circumference: " << getCircumference() << endl
+    << "Area: " << getArea() << endl
+    << "Right angled: " << boolalpha << isRightAngled()<< endl;
+  }
+};
 int main(){
+  try{
+    Triangle triangle1(3,4,5);
+    // triangle1.setTriangle(3,5,7);
+    cout << "A: "<< triangle1.getA() << endl; // bad way to display all information
+    cout << "B: "<< triangle1.getB() << endl;
+    cout << "C: "<< triangle1.getC() << endl;
+    cout << "Circumference: "<< triangle1.getCircumference() << endl;
+    cout << "Area: "<< triangle1.getArea() << endl;
+    cout << "Right angled: "<< boolalpha << triangle1.isRightAngled() << endl; // last row for bad way
+    cout << " " << endl;
+    triangle1.Display(); // better way to display all information for Triangle
+    cout << " " << endl;
+    triangle1.scalingTriangle(5);
+    cout << "After scaling triangle: " << endl;
+    triangle1.Display(); // better way to display all information for Triangle
+  }catch(exception &error){
+    cout << error.what() << endl;
+  }
+
+  /* Bank acc users
+  try
+  {
+    BankAccount user1;
+  
+    user1.Deposit(500);
+    user1.GetBalance();
+    user1.Display();
+    user1.Withdraw(400);
+    user1.GetBalance();
+    user1.Display();
+  }
+  catch(exception &error)
+  {
+    cout << error.what() << endl;
+  }
+  */
+
+  /*  STUDENT CLASS
   try
   {
     Student s1("Kemal", "Skrijelj", 1010); // 3 parameters, because my constructor have 3 parameters
@@ -129,13 +266,13 @@ int main(){
     }else{
       cout << "Student don't passed a year!!" << endl;
     }
-  
+    
     s1.Display();
-  
+    
     s1.transferToNextYear();
-  
+    
     cout << "After transfer to next year: " << endl;
-  
+    
     s1.Display();
     cout << endl;
   }
@@ -143,6 +280,7 @@ int main(){
   {
     cout << "Error: " << error;
   }
+  */
   
   
   /*
